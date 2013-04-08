75f5aad7d9c88c71aa343f3fb43fb6301530438a
Ryan Neufeld <ryan@rkn.io>
2013-04-07 23:08:36 -0400

    Add currency and radices sections

--- project.clj
+++ project.clj
@@ -4,4 +4,5 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [clj-time "0.5.0"]])
+                 [clj-time "0.5.0"]
+                 [clojurewerkz/money "1.1.0"]])
