fadf21e1098766fb21b3887bf65077c42bf17336
Ryan Neufeld <ryan@rkn.io>
2013-08-02 08:05:20 -0600

    Atlas rendering tweaks

--- .compile/project.clj
+++ .compile/project.clj
@@ -2,4 +2,4 @@
   :description "Compile the Clojure Cookbook asciidoc to a format
   consumable by O'Reilly Atlas"
   :main clojure-cookbook.compile
-  :dependencies [[org.clojure/clojure "1.5.0"]])
+  :dependencies [[org.clojure/clojure "1.5.1"]])
