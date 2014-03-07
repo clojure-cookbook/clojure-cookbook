624c13314fe33684544af389462d568e3941a614
Michael Mullis <michael@mullistechnologies.com>
2014-03-07 17:10:19 -0500

    added bootstrap_apt_linux.sh

---
+++ script/asciidoc/bootstrap_apt_linux.sh
@@ -0,0 +1,87 @@
+#!/bin/bash
+
+# Brew-based bootstrap script for setting up an Apt based system to build
+# Clojure Cookbook
+#
+# This script assumes your SOURCE_HIGHLIGHT_DIR is
+# /usr/share/source-highlight. If it isn't, override the environment
+# variable when you run this script:
+#
+#  $ SOURCE_HIGHLIGHT_DIR=/foo/bar ./script/asciidoc/bootstrap_apt_linux.sh
+#
+# Files will not be modified or replaced if they already exist
+#
+# Running this script more than once is OK.
+
+set -e
+
+echo "Begin Setup"
+
+# Install asciidoc
+command -v asciidoc >/dev/null 2>&1 || {
+  echo "Installing asciidoc"
+  sudo apt-get install asciidoc
+}
+
+# Install source-highlight
+command -v source-highlight >/dev/null 2>&1 || {
+  echo "Installing source-highlight"
+  sudo apt-get install source-highlight
+}
+
+# Assume brew is going to use /usr/local/share/source-highlight
+SOURCE_HIGHLIGHT_DIR="${SOURCE_HIGHLIGHT_DIR:-/usr/share/source-highlight}"
+CLOJURE_LANG_FILE="${SOURCE_HIGHLIGHT_DIR}/clojure.lang"
+JSON_LANG_FILE="${SOURCE_HIGHLIGHT_DIR}/json.lang"
+JSON_STYLE_FILE="${SOURCE_HIGHLIGHT_DIR}/json.style"
+PLAIN_LANG_FILE="${SOURCE_HIGHLIGHT_DIR}/plain.lang"
+
+CLOJURE_LANG_SOURCE="https://gist.github.com/alandipert/265810/raw/8dec04317e02187b03e96bb1e0206e154e0ae5e2/clojure.lang"
+JSON_LANG_SOURCE="https://raw.github.com/freeformsystems/rlx/master/highlight/json.lang"
+JSON_STYLE_SOURCE="https://raw.github.com/freeformsystems/rlx/master/highlight/json.style"
+
+# uncomment the following to debug/start fresh
+# does not clean LANG_MAP_FILE
+## rm $CLOJURE_LANG_FILE $JSON_LANG_FILE $JSON_STYLE_FILE $PLAIN_LANG_FILE
+
+# Clojure highlighting support
+test -f "$CLOJURE_LANG_FILE" || (echo "********** Adding $CLOJURE_LANG_FILE **********" && sudo curl --location --silent --output "$CLOJURE_LANG_FILE" "$CLOJURE_LANG_SOURCE")
+
+# JSON highlighting support
+test -f "$JSON_LANG_FILE" || (echo "********** Adding $JSON_LANG_FILE **********" && sudo curl --location --silent --output "$JSON_LANG_FILE" "$JSON_LANG_SOURCE")
+test -f "$JSON_STYLE_FILE" || (echo "********** Adding $JSON_STYLE_FILE **********" && sudo curl --location --silent --output "$JSON_STYLE_FILE" "$JSON_STYLE_SOURCE")
+
+# CSV/text/plain highlighting
+# This is to avoid errors more than actually highlight anything.
+test -f "$PLAIN_LANG_FILE" || (echo "********** Adding $PLAIN_LANG_FILE **********" && sudo cat <<END > "$PLAIN_LANG_FILE")
+include "number.lang"
+include "symbols.lang"
+cbracket = "{|}"
+END
+
+LANG_MAP_FILE="$SOURCE_HIGHLIGHT_DIR/lang.map"
+
+# Add mappings to $SOURCE_HIGHLIGHT_DIR/lang.map
+# DO NOT REMOVE lang.map -- it has other mappings
+grep shell-session "$LANG_MAP_FILE" >/dev/null || {
+  echo "********** Backing up original $LANG_MAP_FILE to ${LANG_MAP_FILE}.bak"
+  sudo cp "$LANG_MAP_FILE" "${LANG_MAP_FILE}.bak"
+
+  echo "********** Appending to $LANG_MAP_FILE"
+  sudo cat <<END >> "$LANG_MAP_FILE"
+
+clojure = clojure.lang
+csv = plain.lang
+json = json.lang
+plain = plain.lang
+shell-session=sh.lang
+END
+
+  echo "Cleaning $LANG_MAP_FILE"
+  cleaned_contents="`sort "$LANG_MAP_FILE" | uniq`"
+  sudo echo "$cleaned_contents" > "$LANG_MAP_FILE"
+
+}
+
+echo "Setup Complete"
+
