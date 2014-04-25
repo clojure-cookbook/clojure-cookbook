216f91a86c80c36b02150f25f0695215ed74ac62
Ryan Neufeld <ryan@rkn.io>
2014-04-25 09:53:44 -0400

    Re-allow overriding source-highlight dir

--- script/asciidoc/bootstrap.sh
+++ script/asciidoc/bootstrap.sh
@@ -24,7 +24,7 @@ set -e
 
 # Determine package installation command
 if [[ -n "$PACKAGE_INSTALL" ]]; then
-  # Do nothing
+  echo "Using '$PACKAGE_INSTALL' as install command"
 elif [[ "$OSTYPE" = "darwin"* ]]; then
   PACKAGE_INSTALL="brew install"
 elif command -v apt-get >/dev/null 2>&1; then
@@ -37,7 +37,7 @@ else
   echo "Unable to determine local package manager. Provide one via \$PACKAGE_INSTALL like \"sudo <my-packager> install\""
 fi
 
-echo "Beginning Setup"
+echo "Beginning Setup..."
 
 # Install asciidoc
 command -v asciidoc >/dev/null 2>&1 || {
@@ -51,16 +51,18 @@ command -v source-highlight >/dev/null 2>&1 || {
   $PACKAGE_INSTALL "source-highlight"
 }
 
-if [[ -d "/usr/local/share/source-highlight" ]] ; then
-    DEFAULT_SOURCE_HIGHLIGHT_DIR="/usr/local/share/source-highlight"
+
+if [[ -n "$SOURCE_HIGHLIGHT_DIR" ]]; then
+  echo "Using source-highlight directory '$SOURCE_HIGHLIGHT_DIR'"
+elif [[ -d "/usr/local/share/source-highlight" ]] ; then
+  SOURCE_HIGHLIGHT_DIR="/usr/local/share/source-highlight"
 elif [[ -d "/usr/share/source-highlight" ]] ; then
-    DEFAULT_SOURCE_HIGHLIGHT_DIR="/usr/share/source-highlight"
+  SOURCE_HIGHLIGHT_DIR="/usr/share/source-highlight"
 else
-    echo "source-highlight directory not found."
-    exit
+  echo "source-highlight directory not found. Provide one via \$SOURCE_HIGHLIGHT_DIR"
+  exit
 fi
 
-SOURCE_HIGHLIGHT_DIR="${SOURCE_HIGHLIGHT_DIR:-$DEFAULT_SOURCE_HIGHLIGHT_DIR}"
 CLOJURE_LANG_FILE="${SOURCE_HIGHLIGHT_DIR}/clojure.lang"
 JSON_LANG_FILE="${SOURCE_HIGHLIGHT_DIR}/json.lang"
 JSON_STYLE_FILE="${SOURCE_HIGHLIGHT_DIR}/json.style"
@@ -75,15 +77,15 @@ JSON_STYLE_SOURCE="https://raw.github.com/freeformsystems/rlx/master/highlight/j
 ## rm $CLOJURE_LANG_FILE $JSON_LANG_FILE $JSON_STYLE_FILE $TEXT_LANG_FILE
 
 # Clojure highlighting support
-test -f "$CLOJURE_LANG_FILE" || (echo "********** Adding $CLOJURE_LANG_FILE **********" && $SUDO curl --location --silent --output "$CLOJURE_LANG_FILE" "$CLOJURE_LANG_SOURCE")
+test -f "$CLOJURE_LANG_FILE" || (echo "** Adding $CLOJURE_LANG_FILE **" && $SUDO curl --location --silent --output "$CLOJURE_LANG_FILE" "$CLOJURE_LANG_SOURCE")
 
 # JSON highlighting support
-test -f "$JSON_LANG_FILE" || (echo "********** Adding $JSON_LANG_FILE **********" && $SUDO curl --location --silent --output "$JSON_LANG_FILE" "$JSON_LANG_SOURCE")
-test -f "$JSON_STYLE_FILE" || (echo "********** Adding $JSON_STYLE_FILE **********" && $SUDO curl --location --silent --output "$JSON_STYLE_FILE" "$JSON_STYLE_SOURCE")
+test -f "$JSON_LANG_FILE" || (echo "** Adding $JSON_LANG_FILE **" && $SUDO curl --location --silent --output "$JSON_LANG_FILE" "$JSON_LANG_SOURCE")
+test -f "$JSON_STYLE_FILE" || (echo "** Adding $JSON_STYLE_FILE **" && $SUDO curl --location --silent --output "$JSON_STYLE_FILE" "$JSON_STYLE_SOURCE")
 
 # CSV/text/text highlighting
 # This is to avoid errors more than actually highlight anything.
-test -f "$TEXT_LANG_FILE" || (echo "********** Adding $TEXT_LANG_FILE **********" && cat <<END | $SUDO tee "$TEXT_LANG_FILE" >/dev/null)
+test -f "$TEXT_LANG_FILE" || (echo "** Adding $TEXT_LANG_FILE **" && cat <<END | $SUDO tee "$TEXT_LANG_FILE" >/dev/null)
 include "number.lang"
 include "symbols.lang"
 cbracket = "{|}"
@@ -96,10 +98,10 @@ LANG_MAP_FILE="$SOURCE_HIGHLIGHT_DIR/lang.map"
 (grep text.lang "$LANG_MAP_FILE" >/dev/null &&
  grep clojure.lang "$LANG_MAP_FILE" >/dev/null &&
  grep shell-session "$LANG_MAP_FILE" >/dev/null ) || {
-  echo "********** Backing up original $LANG_MAP_FILE to ${LANG_MAP_FILE}.bak"
+  echo "** Backing up original $LANG_MAP_FILE to ${LANG_MAP_FILE}.bak"
   $SUDO cp "$LANG_MAP_FILE" "${LANG_MAP_FILE}.bak"
 
-  echo "********** Appending to $LANG_MAP_FILE"
+  echo "** Appending to $LANG_MAP_FILE"
   cat <<END | $SUDO tee -a "$LANG_MAP_FILE" >/dev/null
 
 clojure = clojure.lang
